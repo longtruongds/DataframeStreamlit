8e68d2ee72dc2c742f64e0a61364134aac1210de
longtruongds <86976591+longtruongds@users.noreply.github.com>
2024-04-11 23:40:51 +0700

    Update stExpander.py

--- stExpander.py
+++ stExpander.py
@@ -9,10 +9,6 @@ import streamlit as st
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
 with st.expander("See explanation"):
-    st.write(\"\"\"
-        The chart above shows some numbers I picked for you.
-        I rolled actual dice for these, so they're *guaranteed* to
-        be random.
-    \"\"\")
+    st.subheader("test")
     st.image("https://static.streamlit.io/examples/dice.jpg")
 
