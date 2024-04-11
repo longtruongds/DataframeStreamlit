87392d2458082e605e7a4f3f75a0709d7f4b1ca0
longtruongds <86976591+longtruongds@users.noreply.github.com>
2024-04-11 23:27:13 +0700

    Add files via upload

---
+++ plotMap.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
